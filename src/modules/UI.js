89a2b879aa4a3f54ee6cd29667d270a549f047e7
RossTarrant <rosstarrant3@gmail.com>
2022-03-13 19:32:08 +0000

    Created navbar toggle

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,8 +1,11 @@
+import menuIcon from '../images/menu.svg';
+
 export default class UI{
 
     static initLoad(){
-        this.showNavbar();
         this.showHeader();
+        this.showNavbar();
+        this.showTasks();
     }
 
     static showNavbar(){
@@ -21,14 +24,57 @@ export default class UI{
         content.removeChild(nav)
     }
 
+    static getNavState(){
+        let heading = document.querySelector('header');
+        if(heading===null){
+            return "off"
+        }
+        else{
+            return "on"
+        }
+    }
+
     static showHeader(){
+        let clicks = 0
         const content = document.querySelector('.content');
         const header = document.createElement('div');
         const heading = document.createElement('h1');
         heading.textContent = "Todo List"
-        header.appendChild(heading);
         header.classList.add('header');
+        const menuImg = new Image();
+        menuImg.src = menuIcon;
+        menuImg.addEventListener('click', function(){
+            if(clicks % 2 === 0){
+                UI.hideNavbar();
+                const tasks = document.querySelector('.tasks');
+                tasks.classList.remove('tasks')
+                tasks.classList.add('tasks-fullscreen');
+            }
+            else{
+                const tasks = document.querySelector('.tasks-fullscreen');
+                header.classList.remove('header-fullscreen')
+                header.classList.add('header');
+                tasks.classList.remove('tasks-fullscreen')
+                tasks.classList.add('tasks');
+                UI.showNavbar();
+            }
+            clicks++;
+            console.log(clicks);
+        });
+        header.appendChild(menuImg)
+        header.appendChild(heading);
         content.appendChild(header);
+    }
+
+    static showTasks(){
+        const content = document.querySelector('.content');
+        const tasks = document.createElement('div');
+        tasks.classList.add('tasks');
+        content.appendChild(tasks);
+    }
+
+    static addTodo(){
+
     }
 
 }
